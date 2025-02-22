076ce5842653b350bb82e434e4faa89e87391e70
anasss1 <52884957+anasss1@users.noreply.github.com>
2025-02-22 14:16:16 +0100

    Add files via upload

---
+++ TicketManagementSystem.java
@@ -0,0 +1,14 @@
+package com.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketManagementSystemApplication {
+
+    public static void main(String[] args) {
+        // Start the Spring Boot application
+        SpringApplication.run(TicketManagementSystemApplication.class, args);
+    }
+}
